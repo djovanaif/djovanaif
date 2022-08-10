e337685699453871dc457274855181a5b4a2d2b4
djovanaif <100842531+djovanaif@users.noreply.github.com>
2022-08-10 09:51:35 -0300

    1

---
+++ lista_djovana.py
@@ -0,0 +1,59 @@
+#1acrescente na lista de compras uva e pessego
+
+lista_compras=['banana', 'laranja', 'maçã']
+print(lista_compras)
+
+lista_compras.append("uva")
+lista_compras.append("pessego")
+print(lista_compras)
+
+print(lista_compras[0],lista_compras[2])
+
+lista_recados=("voltar antes das 10am", "comer", "tomar banho","fazer comida")
+recados= len(lista_recados)
+print(recados)
+
+listaA=["yamaha","honda","kawasaki"]
+listaB=["chevrolet","ford","fiat"]
+listaC=["prada","gucci","onça preta"]
+
+print(listaA)
+print(listaB)
+print(listaC)
+
+"listaA(tipos_motos)"
+"listaB(tipos_carros)"
+"listaC(tipos_roupas)"
+
+"tipos_motos.append(tipos_carros)"
+print("tipos_motos")
+
+lista_alunos=["josé","joão","luiz"]
+lista_notas=[8.5,9.2 ,6.7]
+print(lista_alunos)
+print(lista_notas)
+lista_vazia=[10.0]
+print(lista_vazia)
+
+#listacom itens diferentes tipos
+lista_misturada=[17, 8.8, "sorveteria",["pistache", "chocolate"]]
+print(lista_misturada)
+
+#acessando elementos de uma lista
+
+print(lista_notas)
+lista_notas[2]=9.0
+print(lista_notas)
+
+#adicionando itens a uma lista append, extend, remove, insert
+print(lista_alunos)
+lista_alunos.append("pedro")
+print(lista_alunos)
+lista_alunos.insert(1, "luana")
+print(lista_alunos)
+novos_alunos=['bruno', 'gabriel', 'kaua']
+lista_alunos.extend(novos_alunos)
+print(lista_alunos)
+
+lista_alunos1=['pedro', 'luana',]
+lista_alunos2=['bruno','gabriel','kaua']
